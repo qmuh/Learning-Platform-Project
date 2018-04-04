cd825862f9d502c9271a41ee0036e38f2b6be1fe
trvrle <32115579+trvrle@users.noreply.github.com>
2018-04-03 20:40:06 -0600

    Changed appearance of Login GUI

--- src/frontend/LoginGUI.java
+++ src/frontend/LoginGUI.java
@@ -3,61 +3,112 @@ package frontend;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+
 import javax.swing.JPanel;
 import javax.swing.JLabel;
+import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
-import javax.swing.JTree;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.MatteBorder;
 
-public class LoginGUI extends JFrame implements ServerInfo
+import com.sun.org.apache.bcel.internal.generic.NEW;
+
+
+public class LoginGUI extends JFrame implements ServerInfo, WondrisInfo, Colours
 {
 
-	JTextField userName;
-	JTextField password;
-	JButton enterCredentials;
-	private JTextField textField;
-	private JTextField textField_1;
+	private static final long serialVersionUID = 1L;
+	private JTextField userName;
+	private JTextField password;
+	private JButton enterCredentials;
 
-	public LoginGUI()
+	public LoginGUI(String s)
 	{
+		super(s);
 		setSize(1600, 1000);
-		
-		
+		createFields();
+
+		JPanel loginPanel = new JPanel();
+		loginPanel.setBorder(BorderFactory.createCompoundBorder(new EmptyBorder(200, 400, 200, 400),
+				new MatteBorder(5, 5, 5, 5, Color.DARK_GRAY)));
+		loginPanel.add(createLoginPanel(NAME));
+		loginPanel.setBackground(BACKGROUND_COLOUR);
+		add(loginPanel);
 		setVisible(true);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		getContentPane().setLayout(null);
-		
-		JPanel panel = new JPanel();
-		panel.setBounds(170, 72, 510, 334);
-		getContentPane().add(panel);
-		panel.setLayout(null);
-		
-		textField = new JTextField();
-		textField.setBounds(243, 48, 146, 26);
-		panel.add(textField);
-		textField.setColumns(10);
-		
-		JLabel lblUsername = new JLabel("Username");
-		lblUsername.setBounds(113, 45, 95, 32);
-		panel.add(lblUsername);
-		
-		JLabel label = new JLabel("Username");
-		label.setBounds(113, 93, 95, 32);
-		panel.add(label);
-		
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		textField_1.setBounds(243, 96, 146, 26);
-		panel.add(textField_1);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		login();
 	}
 
-	public void login()
+	private void login()
 	{
-
+		
+	}
+	
+	private void createFields() {
+		userName = new JTextField(20);
+		password = new JTextField(20);
+		enterCredentials = new JButton("Sign in");
+	}
+	
+	private JPanel createLoginPanel(String s) 
+	{
+		JPanel thePanel = new JPanel();
+		thePanel.setLayout(new BorderLayout());
+		thePanel.setBackground(TERTIARY_COLOR);
+		thePanel.add(createTitle(s), BorderLayout.NORTH);
+		thePanel.add(createTextPanel(), BorderLayout.CENTER);
+		return thePanel;
 	}
 
+	private JPanel createTextPanel()
+	{
+		JPanel wrapper = new JPanel();
+		JPanel textPanel = new JPanel();
+		textPanel.setLayout(new GridLayout(3, 1));
+		textPanel.add(createTextField("Username", userName));
+		textPanel.add(createTextField("Password", password));
+		textPanel.add(createButtonPanel());
+		wrapper.add(textPanel);
+		return wrapper;
+	}
+
+	private JPanel createTitle(String s) {
+		JPanel titlePanel = new JPanel();
+		titlePanel.setBackground(SECONDARY_COLOR);
+		titlePanel.setPreferredSize(new Dimension(760, 50));
+		JLabel title = new JLabel(s);
+		title.setFont(TITLE_FONT);
+		title.setHorizontalAlignment(SwingConstants.CENTER);
+		titlePanel.add(title);
+		return titlePanel;
+	}
+	
+	private JPanel createTextField(String s, JTextField field) {
+		JPanel textFieldPanel = new JPanel();
+		JLabel text = new JLabel(s);
+		text.setFont(TEXTFONT);
+		textFieldPanel.add(text);
+		textFieldPanel.add(field);
+		return textFieldPanel;
+	}
+	
+	private JPanel createButtonPanel() {
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setPreferredSize(new Dimension(50, 25));
+		buttonPanel.add(enterCredentials);
+		return buttonPanel;
+	}
+	
 	public static void main(String[] args)
 	{
-		new LoginGUI();
+		new LoginGUI(TITLE);
 	}
 }
---
+++ src/frontend/WondrisInfo.java
@@ -0,0 +1,19 @@
+package frontend;
+
+import java.awt.Font;
+
+public interface WondrisInfo
+{
+	public static final String TITLE = "Wondris Learning Platform Login";
+	
+	public static final String NAME = "Wondris";
+	
+	public static final int TITLE_FONT_SIZE = 36;
+	
+	public static final int TEXT_FONT_SIZE = 16;
+	
+	public static final Font TITLE_FONT = new Font("Century Gothic", Font.PLAIN, TITLE_FONT_SIZE);
+	
+	public static final Font TEXTFONT = new Font("Century Gothic", Font.PLAIN, TEXT_FONT_SIZE);
+
+}

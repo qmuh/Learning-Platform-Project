3738789d84ab38ed9f5f09d29a99ff4e35da27a1
trvrle <32115579+trvrle@users.noreply.github.com>
2018-04-05 16:42:51 -0600

    Exposed JCheckBox

--- src/frontend/view/pages/items/CourseItem.java
+++ src/frontend/view/pages/items/CourseItem.java
@@ -1,24 +1,42 @@
 package frontend.view.pages.items;
 
-import java.awt.GridLayout;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JLabel;
 
+import frontend.interfaces.WondrisInfo;
 import sharedobjects.Course;
 
-public class CourseItem extends Box
+public class CourseItem extends Box implements WondrisInfo
 {
 
 	private Course course;
+	private JCheckBox active;
+	private JButton view;
 	public CourseItem(Course course)
 	{
 		super(BoxLayout.X_AXIS);
 		this.course = course;
-		
-		this.add(new JLabel(course.getName()));
-		this.add(new JButton("View"));
+		this.add(createCheckBox());
+		this.add(createLabel(course.getName()));
+		this.add(createButton("View"));
+	}
+	private JCheckBox createCheckBox() {
+		active = new JCheckBox();
+		active.setSelected(course.getActive());
+		return active;
+	}
+	private JLabel createLabel(String name) {
+		JLabel label = new JLabel(name);
+		label.setFont(TEXTFONT);
+		return label;
+	}
+	private JButton createButton(String name) {
+		view = new JButton(name);
+		view.setFont(TEXTFONT);
+		return view;
 	}
 }

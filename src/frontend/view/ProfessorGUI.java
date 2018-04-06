903c0224dc7f808610e252cf9a42b5f379a53338
trvrle <32115579+trvrle@users.noreply.github.com>
2018-04-06 11:36:30 -0600

    Implemented CourseNavBar Functionality

--- src/frontend/view/ProfessorGUI.java
+++ src/frontend/view/ProfessorGUI.java
@@ -74,6 +74,7 @@ public class ProfessorGUI extends PageNavigator
 					createCourseItem(course, homePage);
 
 					createEnrollmentPage(course);
+					createAssignmentPage(course);
 
 					System.out.println(
 							"Course name is: " + coursesList.get(i).getName());
@@ -102,6 +103,9 @@ public class ProfessorGUI extends PageNavigator
 	private CoursePage createCoursePage(Course course)
 	{
 		CoursePage coursePage = new CoursePage(course);
+		coursePage.setEnrollmentButtonListener(new EnrollmentButtonListener(course));
+		coursePage.setAssignmentButtonListener(new AssignmentButtonListener(course));
+		coursePage.setGradesButtonListener(new GradesButtonListener(course));
 		this.addPage(coursePage, coursePage.getName());
 		return coursePage;
 	}
@@ -109,14 +113,18 @@ public class ProfessorGUI extends PageNavigator
 	private void createAssignmentPage(Course course)
 	{
 		AssignmentPage assignmentPage = new AssignmentPage(course);
+		assignmentPage.setEnrollmentButtonListener(new EnrollmentButtonListener(course));
+		assignmentPage.setAssignmentButtonListener(new AssignmentButtonListener(course));
+		assignmentPage.setGradesButtonListener(new GradesButtonListener(course));
 		this.addPage(assignmentPage, assignmentPage.getName());
 	}
 	
-	
-
 	private void createEnrollmentPage(Course course)
 	{
 		EnrollmentPage enrollmentPage = new EnrollmentPage(course);
+		enrollmentPage.setEnrollmentButtonListener(new EnrollmentButtonListener(course));
+		enrollmentPage.setAssignmentButtonListener(new AssignmentButtonListener(course));
+		enrollmentPage.setGradesButtonListener(new GradesButtonListener(course));
 		this.addPage(enrollmentPage, enrollmentPage.getName());
 		enrollmentPage.setSearchButtonListener(
 				new SearchButtonListener(enrollmentPage));
@@ -282,6 +290,52 @@ public class ProfessorGUI extends PageNavigator
 			showPage(COURSE_PAGE + courseId);
 		}
 
+	}
+	
+	private class EnrollmentButtonListener implements ActionListener
+	{
+		private Course course;
+		public EnrollmentButtonListener(Course course)
+		{
+			this.course = course;
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent e)
+		{
+			showPage(ENROLLMENT_PAGE+course.getId());
+		}
+		
+	}
+	private class AssignmentButtonListener implements ActionListener
+	{
+		private Course course;
+		public AssignmentButtonListener(Course course)
+		{
+			this.course = course;
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent e)
+		{
+			showPage(ASSIGNMENT_PAGE+course.getId());
+		}
+		
+	}
+	private class GradesButtonListener implements ActionListener
+	{
+		private Course course;
+		public GradesButtonListener(Course course)
+		{
+			this.course = course;
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent e)
+		{
+			showPage(GRADES_PAGE+course.getId());
+		}
+		
 	}
 
 	private AssignmentPage createAssignmentPage()

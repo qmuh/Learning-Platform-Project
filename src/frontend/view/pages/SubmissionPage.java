89e782a0f17eb5ea3e815ddd25dc055fb6174680
trvrle <32115579+trvrle@users.noreply.github.com>
2018-04-10 15:55:27 -0600

    Created the Submission GUI Portion
    Co-Authored-By: Jimmy W. Truong <jimmy.truong@ucalgary.ca>

--- src/frontend/view/pages/SubmissionPage.java
+++ src/frontend/view/pages/SubmissionPage.java
@@ -2,6 +2,7 @@ package frontend.view.pages;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.util.HashMap;
 import java.util.Vector;
 
 import javax.swing.BorderFactory;
@@ -16,11 +17,13 @@ import javax.swing.border.TitledBorder;
 import javax.swing.WindowConstants;
 
 import frontend.view.pages.components.BoxList;
+import frontend.view.pages.items.AssignSubItem;
 import frontend.view.pages.items.CourseItem;
 import frontend.view.pages.items.GeneralItem;
 import frontend.view.pages.items.SubmitItem;
 import shared.objects.Assignment;
 import shared.objects.Course;
+import shared.objects.Student;
 import shared.objects.Submission;
 
 /**
@@ -30,15 +33,18 @@ import shared.objects.Submission;
  * @version 1.0
  * @since April 6, 2018
  */
-public class SubmissionPage extends CoursePage<SubmitItem, Submission>
+public class SubmissionPage extends CoursePage<AssignSubItem, Submission>
 {
-
+	private HashMap<Integer, AssignSubItem> assignmentMap;
+	
 	public SubmissionPage(Course course)
 	{
 		super(course);
 		this.setName(SUBMISSION_PAGE + course.getId());
 		this.setPageTitle("Submissions");
-
+		
+		this.assignmentMap = new HashMap<Integer, AssignSubItem>();
+		
 		bodyCenter.add(createSubmissionPage(), BorderLayout.CENTER);
 	}
 
@@ -47,34 +53,32 @@ public class SubmissionPage extends CoursePage<SubmitItem, Submission>
 		JPanel view = new JPanel();
 		view.setLayout(new BoxLayout(view, BoxLayout.Y_AXIS));
 
-		
-
 		JScrollPane submissionScrollPane = new JScrollPane(view);
 		submissionScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-//		submissionScrollPane.setBorder(BorderFactory.createEmptyBorder(80, 80, 80, 80));
 		return submissionScrollPane;
 	}
 	
-	public void setBoxList(BoxList<SubmitItem> itemDisplay)
+//	@Override
+//	public void setBoxList(BoxList<SubmitItem> itemDisplay)
+//	{
+//		JScrollPane scrollPane = new JScrollPane(itemDisplay);
+//		scrollPane.setVerticalScrollBarPolicy(
+//				ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+//		scrollPane.getVerticalScrollBar().setUnitIncrement(20);
+//		bodyCenter.add(scrollPane, BorderLayout.CENTER);
+//	}
+
+
+	public void addAssignment(Assignment assignment, Vector<Student> classList)
 	{
-		JScrollPane scrollPane = new JScrollPane(itemDisplay);
-		scrollPane.setVerticalScrollBarPolicy(
-				ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
-		scrollPane.getVerticalScrollBar().setUnitIncrement(20);
-		bodyCenter.add(scrollPane, BorderLayout.CENTER);
+		AssignSubItem assignmentItem = new AssignSubItem(assignment, classList); 
+		this.assignmentMap.put(assignment.getId(), assignmentItem);
+		this.itemDisplay.add(assignmentItem);
 	}
-
-
-	private JPanel createSubmission()
+	
+	public void addSubmission(Submission submission)
 	{
-		JPanel submissionPanel = new JPanel();
-		submissionPanel.add(createLabel("Default", TEXT_FONT));
-		return submissionPanel;
-	}
-
-	public void addAssignment(Assignment assignment)
-	{
-
+		this.assignmentMap.get(submission.getAssign_id()).addSubmission(submission);
 	}
 
 	@Override
@@ -88,6 +92,9 @@ public class SubmissionPage extends CoursePage<SubmitItem, Submission>
 	{
 		JFrame frame = new JFrame();
 		SubmissionPage submissionPage = new SubmissionPage(new Course(1010101, "ENGG 201", true));
+		
+		
+		
 		frame.add(submissionPage);
 		frame.setSize(1600, 1000);
 		frame.setVisible(true);
